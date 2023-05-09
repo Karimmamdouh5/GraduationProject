d153f210854576bf5370b3d6ed78031ad54507da
shehabhoss123 <shehabhossam25259@gmail.com>
2023-05-10 00:24:14 +0200

    buildyourpc

---
+++ src/app/Classes/builtpc.spec.ts
@@ -0,0 +1,7 @@
+import { Builtpc } from './builtpc';
+
+describe('Builtpc', () => {
+  it('should create an instance', () => {
+    expect(new Builtpc()).toBeTruthy();
+  });
+});
---
+++ src/app/Classes/builtpc.ts
@@ -0,0 +1,21 @@
+import { Product } from "./product";
+
+export class Builtpc extends Product 
+{
+    
+/*public Processor :Product = new Product();
+public motherboard :Product = new Product();
+public ram :Product = new Product();
+public gpu :Product = new Product();
+public Powersupply :Product = new Product();
+public Storage :Product = new Product();
+public Case :Product = new Product();
+public Monitor :Product = new Product();
+public Cooler :Product = new Product();
+*/
+public ProductsList: Array<Product> =[];
+
+}
+//MainComponents=['Processor','Motherboard','GPU','Powersupply','Ram','Storage','Case','Monitor','Cooler'];
+
+
