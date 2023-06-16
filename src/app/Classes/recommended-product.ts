e9d8f67724a8c992ce345c4d9f4da08793fbb5e1
Karimmamdouh5 <karimmamdoh5@gmail.com>
2023-06-16 16:38:05 +0300

    editRecommendedProductclass

--- src/app/Classes/recommended-product.ts
+++ src/app/Classes/recommended-product.ts
@@ -1,10 +1,10 @@
 import { ShopProduct } from "./ShopProduct";
-import { Category } from "d:/My Private Files/Karimmamdouh5-master1/src/app/Classes/category";
+import { Category } from "./category";
 
 export class RecommendedProduct extends ShopProduct 
 {
   isChecked=false;
-  override category: import("d:/My Private Files/Karimmamdouh5-master1/src/app/Classes/category").Category=new Category();
+  override category: Category=new Category();
   override description: string='';
   override imageUrl: string='';
   override name: string='';
